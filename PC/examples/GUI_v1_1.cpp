9b27a3dc2c33b6f8ea0f6975a71e05ea38973cbb
MathMork <maththemoe@gmail.com>
2022-11-02 13:30:13 +0100

    Updated filepath coffee image

--- PC/examples/GUI_v1_1.cpp
+++ PC/examples/GUI_v1_1.cpp
@@ -22,7 +22,7 @@ int main(int argc, const char *argv[])
     // Matrix to put the captured image in
     cv::Mat Img{};
     // Matrix with picture in it
-    cv::Mat img_coffee_ready = cv::imread("coffee_ready.jpg", cv::IMREAD_COLOR);
+    cv::Mat img_coffee_ready = cv::imread("resources/coffee_ready.jpg", cv::IMREAD_COLOR);
 
     // Start default camera for testing
     auto cam = cv::VideoCapture(0, cv::CAP_DSHOW);
