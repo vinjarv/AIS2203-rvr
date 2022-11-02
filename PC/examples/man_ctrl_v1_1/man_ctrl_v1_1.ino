78804f28f375de8409aa826fd73070cc2dc79a46
MathMork <maththemoe@gmail.com>
2022-11-02 17:28:23 +0100

    Added btn to man_ctrl

--- PC/examples/man_ctrl_v1_1/man_ctrl_v1_1.ino
+++ PC/examples/man_ctrl_v1_1/man_ctrl_v1_1.ino
@@ -1,10 +1,14 @@
 #include <Adafruit_MPU6050.h>
 
-
+#define btnPin 1;
 Adafruit_MPU6050 MPUsensor;
+
+int btn;
 bool debug = false;
 
 void setup() {
+  pinMode(btnPin, INPUT);
+  
   Serial.begin(9600);
   while(!Serial){
     delay(10);
@@ -24,19 +28,32 @@ void setup() {
   if(debug) Serial.println("MPU6050 sensor limits set");
 
   Serial.println("");
-  Serial.println("x,\t y,\t z");
+  Serial.println("x,\t y,\t z,\t btn");
   delay(100);
 }
 
 void loop() {
   sensors_event_t acc, gyro, temp;
   MPUsensor.getEvent(&acc, &gyro, &temp);
+  btn = digitalRead(btnPin);
   
   // Writing to serial monitor
+  /*
   Serial.print(acc.acceleration.x);
   Serial.print(",\t");
   Serial.print(acc.acceleration.y);
   Serial.print(",\t");
   Serial.print(acc.acceleration.z);
-  Serial.println(",\t");
+  Serial.print(",\t");
+  Serial.println(btn);
+  */
+
+  Serial.print(gyro.gyro.x);
+  Serial.print(",\t");
+  Serial.print(gyro.gyro.y);
+  Serial.print(",\t");
+  Serial.print(gyro.gyro.z);
+  Serial.print(",\t");
+  Serial.print(btn);
+  
 }
